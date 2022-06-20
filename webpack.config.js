d6c24b74b9e1dc57978c5fb08ba9c0ca8eee272a
Artsem Mukha <artsemmukha@gmail.com>
2022-06-20 15:34:40 +0300

    feat(git): added action for git

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
-const devServer = (isDev) =>
-    !isDev
-        ? {}
-        : {
-              devServer: {
-                  open: true,
-                  hot: false,
-                  port: 8080,
-              },
-          };
+// const devServer = (isDev) =>
+//     !isDev
+//         ? {}
+//         : {
+//               devServer: {
+//                   open: true,
+//                   hot: false,
+//                   port: 8080,
+//               },
+//           }
 
 module.exports = ({ development }) => ({
-    mode: development ? "development" : "production",
-    devtool: development ? "inline-source-map" : false,
+    mode: development ? 'development' : 'production',
+    devtool: development ? 'inline-source-map' : false,
     entry: {
-        main: "./src/index.js",
+        main: './src/index.js',
     },
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].[contenthash].js",
-        publicPath: "/",
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        publicPath: '/',
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
                 test: /\.s[ac]ss$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "postcss-loader",
-                    "sass-loader",
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
                 ],
             },
         ],
@@ -49,19 +49,24 @@ module.exports = ({ development }) => ({
         maxAssetSize: 512000,
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src/index.html"),
+            template: path.join(__dirname, 'src/index.html'),
         }),
         new CopyPlugin({
             patterns: [
-                { from: "./src/assets/images/", to: "public/images" },
-                { from: "./src/assets/static/", to: "" },
-                { from: "./src/app-ads.txt", to: "" },
+                { from: './src/assets/images/', to: 'public/images' },
+                { from: './src/assets/static/', to: '' },
+                { from: './src/app-ads.txt', to: '' },
                 // { from: "./src/EULA", to: "" },
             ],
         }),
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     ],
-    ...devServer(development),
-});
+    // ...devServer(development),
+    devServer: {
+        // open: true,
+        hot: true,
+        port: 8080,
+    },
+})
